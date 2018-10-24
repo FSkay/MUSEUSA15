83cabfa4a36499f9bef92c502df1a9dfe65655a8
FSkay <felipe_menegat@hotmail.com>
2018-10-24 16:17:20 -0300

    2

---
+++ app/src/main/java/com/example/dicossoftware/museusa15/AdicionarArtefatoActivity.java
@@ -0,0 +1,17 @@
+package com.example.dicossoftware.museusa15;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class AdicionarArtefatoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adicionar_artefato);
+    }
+    public void AdicionarArtefato (View v){
+
+    }
+}
